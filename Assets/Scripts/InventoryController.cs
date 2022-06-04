5e775557e1330c28e36c358a90b81745bdc3e9a3
KerstinHung <55979198+KerstinHung@users.noreply.github.com>
2022-06-04 23:09:11 +0800

    Inventory: Overlap exception, Hilight, Insert item

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -7,24 +7,56 @@ public class InventoryController : MonoBehaviour
     [HideInInspector]
     public ItemGrid selectedItemGrid;
 
+    public ItemGrid SelectedItemGrid 
+    { 
+        get => selectedItemGrid; 
+        set
+        {
+            selectedItemGrid = value;
+            inventoryHighlight.SetParent(value); 
+        }
+    }
+
     InventoryItem selectedItem;
+    InventoryItem overlapItem;
     RectTransform rectTransform;
 
     [SerializeField] List<ItemData> items;
     [SerializeField] GameObject itemPrefab;
     [SerializeField] Transform canvasTransform;
 
+    InventoryHighlight inventoryHighlight;
+
+    private void Awake()
+    {
+        inventoryHighlight = GetComponent<InventoryHighlight>();
+    }
+
     private void Update()
     {
         ItemIconDrag();
         
         // not select any grid now
-        if(selectedItemGrid == null) { return; }
+        if(selectedItemGrid == null) 
+        {
+            inventoryHighlight.Show(false);
+            return; 
+        }
+
+        HandleHighlight();
 
         // for test
         if(Input.GetKeyDown(KeyCode.Q))
         {
-            CreateRandomItem();
+            if(selectedItem == null)
+            {
+                CreateRandomItem();
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            InsertRandomItem();
         }
 
         if(Input.GetMouseButtonDown(0))
@@ -38,6 +70,48 @@ public class InventoryController : MonoBehaviour
             
         }
     }
+
+    Vector2Int oldPosition;
+    InventoryItem itemToHighlight;
+    private void HandleHighlight()
+    {
+        Vector2Int positionOnGrid = GetTileGridPosition();
+
+        if(oldPosition == positionOnGrid) { return; }
+
+        oldPosition = positionOnGrid;
+
+        if(selectedItem == null)
+        {
+            itemToHighlight = selectedItemGrid.GetItem(positionOnGrid.x, positionOnGrid.y);
+            if(itemToHighlight != null)
+            {
+                inventoryHighlight.Show(true);
+                inventoryHighlight.SetSize(itemToHighlight);
+                //inventoryHighlight.SetParent(selectedItemGrid);
+                inventoryHighlight.SetPosition(selectedItemGrid, itemToHighlight);
+            }
+            else
+            {
+                inventoryHighlight.Show(false);
+            }
+            
+        }
+        else
+        {
+            inventoryHighlight.Show(selectedItemGrid.BoundryCheck(
+                positionOnGrid.x, 
+                positionOnGrid.y, 
+                selectedItem.itemData.width, 
+                selectedItem.itemData.height)
+                );
+            inventoryHighlight.SetSize(selectedItem);
+            inventoryHighlight.SetParent(selectedItemGrid);
+            inventoryHighlight.SetPosition(selectedItemGrid, selectedItem, positionOnGrid.x, positionOnGrid.y);
+        }
+    }
+
+    // Visualization of item drag
     private void ItemIconDrag()
     {
         if(selectedItem != null)
@@ -59,13 +133,32 @@ public class InventoryController : MonoBehaviour
     }
     private void LeftMouseButtonRelease()
     {
-        Vector2Int tileGridPosition = selectedItemGrid.GetTileGridPosition(Input.mousePosition);
+        
+        Vector2Int tileGridPosition = GetTileGridPosition();
 
         if(selectedItem != null)
         {
             PlaceItem(tileGridPosition);
         }
     }
+
+    private Vector2Int GetTileGridPosition()
+    {
+        Vector2 position = Input.mousePosition;
+
+        // need?
+        if(selectedItem != null)
+        {
+            position.x -= (selectedItem.itemData.width - 1) * ItemGrid.tileSizeWidth / 2;
+            position.y += (selectedItem.itemData.height - 1) * ItemGrid.tileSizeHeight / 2;
+        }
+        // need?
+
+        return selectedItemGrid.GetTileGridPosition(position);
+
+        // Vector2Int tileGridPosition = selectedItemGrid.GetTileGridPosition(position);
+    }
+
     private void PickUpItem(Vector2Int tileGridPosition)
     {
         selectedItem = selectedItemGrid.PickUpItem(tileGridPosition.x, tileGridPosition.y);
@@ -76,8 +169,26 @@ public class InventoryController : MonoBehaviour
     }
     private void PlaceItem(Vector2Int tileGridPosition)
     {
-        selectedItemGrid.PlaceItem(selectedItem, tileGridPosition.x, tileGridPosition.y);
-        selectedItem = null;
+        bool complete = selectedItemGrid.PlaceItem(selectedItem, tileGridPosition.x, tileGridPosition.y, ref overlapItem);
+
+        if(complete)
+        {
+            selectedItem = null;
+            if(overlapItem != null)
+            {
+                selectedItem = overlapItem;
+                overlapItem = null;
+                rectTransform = selectedItem.GetComponent<RectTransform>();
+                rectTransform.SetAsLastSibling();
+            }
+        }
+        /*
+        else
+        {
+            complete = selectedItemGrid.PlaceItem(selectedItem, tileGridPosition.x, tileGridPosition.y);
+        }
+        */
+        
     }
 
     private void CreateRandomItem()
@@ -87,9 +198,28 @@ public class InventoryController : MonoBehaviour
 
         rectTransform = inventoryItem.GetComponent<RectTransform>();
         rectTransform.SetParent(canvasTransform);
+        rectTransform.SetAsLastSibling();
 
         int selectedItemID = UnityEngine.Random.Range(0, items.Count);
         inventoryItem.Set(items[selectedItemID]);
+    }
+
+    private void InsertRandomItem()
+    {
+        if(selectedItemGrid == null) { return; }
+        CreateRandomItem();
+        InventoryItem itemToInsert = selectedItem;
+        selectedItem = null;
+        InsertItem(itemToInsert);
+    }
+
+    private void InsertItem(InventoryItem itemToInsert)
+    {
+        Vector2Int? posOnGrid = selectedItemGrid.FindSpaceForObject(itemToInsert);
+
+        if(posOnGrid == null) { return; }
+
+        selectedItemGrid.PlaceItem(itemToInsert, posOnGrid.Value.x, posOnGrid.Value.y);
     }
 }
 
--- Assets/Scripts/ItemGrid.cs
+++ Assets/Scripts/ItemGrid.cs
@@ -23,8 +23,25 @@ public class ItemGrid : MonoBehaviour
     public InventoryItem PickUpItem(int x, int y)
     {
         InventoryItem toReturn = inventoryItemSlot[x, y];
-        inventoryItemSlot[x, y] = null;
+
+        if(toReturn == null) {return null;}
+
+        CleanGridReference(toReturn);
+
         return toReturn;
+    }
+
+    private void CleanGridReference(InventoryItem item)
+    {
+        // need?
+        for(int ix = 0; ix < item.itemData.width; ix++)
+        {
+            for(int iy = 0; iy < item.itemData.height; iy++)
+            {
+                inventoryItemSlot[item.onGridPositionX + ix, item.onGridPositionY + iy] = null;
+            }
+        }
+        // inventoryItemSlot[x, y] = null;
     }
 
     private void Init(int width, int height)
@@ -32,6 +49,11 @@ public class ItemGrid : MonoBehaviour
         inventoryItemSlot = new InventoryItem[width, height];
         Vector2 size = new Vector2(width * tileSizeWidth, height * tileSizeHeight);
         rectTransform.sizeDelta = size;
+    }
+
+    internal InventoryItem GetItem(int x, int y)
+    {
+        return inventoryItemSlot[x, y];
     }
 
     Vector2 positionOnTheGrid = new Vector2();
@@ -49,17 +71,143 @@ public class ItemGrid : MonoBehaviour
         return tileGridPosition;
     }
 
+    public bool PlaceItem(InventoryItem inventoryItem, int posX, int posY, ref InventoryItem overlapItem)
+    {
+        if(BoundryCheck(posX, posY, inventoryItem.itemData.width, inventoryItem.itemData.height) == false)
+        {
+            return false;
+        }
+
+        // need?
+        if(OverlapCheck(posX, posY, inventoryItem.itemData.width, inventoryItem.itemData.height, ref overlapItem) == false)
+        {
+            overlapItem = null;
+            return false;
+        }
+
+        if(overlapItem != null)
+        {
+            CleanGridReference(overlapItem);
+        }
+
+        PlaceItem(inventoryItem, posX, posY);
+
+        return true;
+    }
+
     public void PlaceItem(InventoryItem inventoryItem, int posX, int posY)
     {
         // the position of a single grid
         RectTransform rectTransform = inventoryItem.GetComponent<RectTransform>();
         rectTransform.SetParent(this.rectTransform);
-        inventoryItemSlot[posX, posY] = inventoryItem;
 
+        // need? 
+        for(int x = 0; x < inventoryItem.itemData.width; x++)
+        {
+            for(int y = 0; y < inventoryItem.itemData.height; y++)
+            {
+                inventoryItemSlot[posX + x, posY + y] = inventoryItem;
+            }
+        }
+        // inventoryItemSlot[posX, posY] = inventoryItem;
+
+        inventoryItem.onGridPositionX = posX;
+        inventoryItem.onGridPositionY = posY;
+
+        Vector2 position = CalculatePositionOnGrid(inventoryItem, posX, posY);
+
+        rectTransform.localPosition = position;
+    }
+
+    public Vector2 CalculatePositionOnGrid(InventoryItem inventoryItem, int posX, int posY)
+    {
         Vector2 position = new Vector2();
         position.x = posX * tileSizeWidth;
         position.y = posY * tileSizeWidth;
+        return position;
+    }
 
-        rectTransform.localPosition = position;
+    private bool OverlapCheck(int posX, int posY, int width, int height, ref InventoryItem overlapItem)
+    {
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                if(inventoryItemSlot[posX+x, posY+y] != null)
+                {
+                    if(overlapItem == null)
+                    {
+                        overlapItem = inventoryItemSlot[posX + x, posY + y];
+                    }
+                    else
+                    {
+                        if(overlapItem != inventoryItemSlot[posX + x, posY + y]) 
+                        {
+                            return false;
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private bool CheckAvailableSpace(int posX, int posY, int width, int height)
+    {
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                if(inventoryItemSlot[posX+x, posY+y] != null)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool PositionCheck(int posX, int posY)
+    {
+        if(posX < 0 || posY < 0)
+        {
+            return false;
+        }
+
+        if(posX >= gridSizeWidth || posY >= gridSizeHeight)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool BoundryCheck(int posX, int posY, int width, int height)
+    {
+        if(PositionCheck(posX, posY) == false) { return false; }
+
+        posX += width-1;
+        posY += height-1;
+
+        if(PositionCheck(posX, posY) == false) { return false; }
+
+        return true;
+    }
+
+    // Add '?' is for nullable
+    internal Vector2Int? FindSpaceForObject(InventoryItem itemToInsert)
+    {
+        for(int x = 0; x < gridSizeWidth; x++)
+        {
+            if(CheckAvailableSpace(x, 0, itemToInsert.itemData.width, itemToInsert.itemData.height) == true)
+            {
+                return new Vector2Int(x, 0);
+            }
+        }
+
+        return null;
     }
 }
