67376b61cec765c9c040b8c8ab056298dc694ecb
LoySami <kmes9940221@gmail.com>
2022-06-05 03:12:32 +0800

    add buliding

--- Assets/Scripts/FtriggerController.cs
+++ Assets/Scripts/FtriggerController.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -32,3 +33,39 @@ public class FtriggerController : MonoBehaviour
         pressF.gameObject.SetActive(false);
     }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FtriggerController : MonoBehaviour
+{
+    public TextMesh pressF;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressF.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pressF.gameObject.activeSelf)
+        {
+            if(Input.GetKey(KeyCode.F))
+            {
+                Destroy(this.gameObject);
+            }    
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        pressF.gameObject.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        pressF.gameObject.SetActive(false);
+    }
+}
+>>>>>>> f1c7fa5... --all
